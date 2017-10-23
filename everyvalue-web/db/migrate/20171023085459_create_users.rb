4f977273d23c781608b5d12fef48553235602dd3
jinsunyang <jinsundev@gmail.com>
2017-10-23 22:01:22 +0900

    User모델 마이그레이션 하고 devise 를 적용하다.

--- everyvalue-web/db/migrate/20171023085459_create_users.rb
+++ everyvalue-web/db/migrate/20171023085459_create_users.rb
@@ -1,15 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :nickname
-      t.string :phone
+      t.string :name, null: false
+      t.string :nickname, null: false
+      # t.string :phone, null: false
       t.integer :gender, limit: 1
       t.date :birth_date
-      t.string :identity_provider
-      t.boolean :is_authorized
+      t.string :identity_provider, default: 'own'
+      # t.boolean :is_authorized, default: false, null: false
 
       t.timestamps
     end
+
+    add_index :users, :name, unique: true
+    add_index :users, :nickname, unique: true
+    # add_index :users, :phone, unique: true
   end
 end
