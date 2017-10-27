1cbb6f07ad6924607e54cd8adcd8a032b084a013
jinsunyang <jinsundev@gmail.com>
2017-10-27 18:46:20 +0900

    active_record에 attr_encoded가 적용되도록 하다.

--- everyvalue-web/app/models/application_record.rb
+++ everyvalue-web/app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+  #include는 module의 method를 instance method로 추가할 때, extend는 module의 method 를 class method로 추가할 때
+  extend ActiveRecordExtension
+
   self.abstract_class = true
 end
