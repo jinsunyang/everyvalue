8e1bffc8ad28ab602d9359b5518b7833d80023e6
jinsunyang <jinsundev@gmail.com>
2017-11-09 01:53:00 +0900

    route 에서 필요없는 항목 삭제

--- everyvalue-web/config/routes.rb
+++ everyvalue-web/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :subject_attachments
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  resources :subjects
   resources :valuations
   resources :hashtags
-  resources :subjects
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
