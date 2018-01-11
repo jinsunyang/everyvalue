6e2ef1046f28310e0ffc06f4b308a4f79b65b58c
jinsunyang <jinsundev@gmail.com>
2018-01-11 18:53:06 +0900

    subject show view가 정상 동작하도록 수정하다.

--- everyvalue-web/app/controllers/comments_controller.rb
+++ everyvalue-web/app/controllers/comments_controller.rb
@@ -28,14 +28,15 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+
+    if @comment.save
+      # format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+      # format.json { render :show, status: :created, location: @comment }
+      render json: nil
+    else
+      # format.html { render :new }
+      # format.json { render json: @comment.errors, status: :unprocessable_entity }
+      render json: nil
     end
   end
 
